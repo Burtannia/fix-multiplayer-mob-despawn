07c595b57b1611d844a2cb7e3f43cf19756b3600
James Burton <burtannia1997@gmail.com>
2024-04-15 14:51:42 +0100

    Initial commit

---
+++ src/main/kotlin/com/burtannia/fixmpdespawn/FixMultiplayerMobDespawn.kt
@@ -0,0 +1,15 @@
+package com.burtannia.fixmpdespawn
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object FixMultiplayerMobDespawn : ModInitializer {
+    private val logger = LoggerFactory.getLogger("fix-multiplayer-mob-despawn")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
