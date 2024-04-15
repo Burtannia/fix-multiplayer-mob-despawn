a74a334e49d848ae554938c9a08924e643ae836f
James Burton <jamesejburton@gmail.com>
2024-04-15 16:34:28 +0100

    Remove client code

--- src/client/kotlin/com/burtannia/fixmpdespawn/FixMultiplayerMobDespawnClient.kt
+++
@@ -1,9 +0,0 @@
-package com.burtannia.fixmpdespawn
-
-import net.fabricmc.api.ClientModInitializer
-
-object FixMultiplayerMobDespawnClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
