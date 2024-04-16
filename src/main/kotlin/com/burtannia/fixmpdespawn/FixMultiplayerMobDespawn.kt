3b7cf636f1042a7d3324ee49b10c0269df365231
James Burton <jamesejburton@gmail.com>
2024-04-16 12:09:03 +0100

    Persist on move world handler

--- src/main/kotlin/com/burtannia/fixmpdespawn/FixMultiplayerMobDespawn.kt
+++ src/main/kotlin/com/burtannia/fixmpdespawn/FixMultiplayerMobDespawn.kt
@@ -1,15 +1,36 @@
 package com.burtannia.fixmpdespawn
 
+import com.burtannia.fixmpdespawn.event.EntityMoveWorldEvent
+import com.burtannia.fixmpdespawn.event.EntityMoveWorldHandler
+import com.burtannia.fixmpdespawn.mixin.MobEntityPersistentDurationMixin
 import net.fabricmc.api.ModInitializer
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.ActionResult
 import org.slf4j.LoggerFactory
 
 object FixMultiplayerMobDespawn : ModInitializer {
-    private val logger = LoggerFactory.getLogger("fix-multiplayer-mob-despawn")
+    private const val MOD_ID: String = "fix-multiplayer-mob-despawn"
+    private val logger = LoggerFactory.getLogger(MOD_ID)
+    private const val PERSIST_DURATION: Long = 60 * 1000 // 60 seconds
 
-	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
-	}
+    override fun onInitialize() {
+        logger.info("Initialising $MOD_ID...")
+        registerHandlers()
+        logger.info("Finished initialising $MOD_ID")
+    }
+
+    private fun registerHandlers() {
+        EntityMoveWorldHandler.EVENT.register(keepAliveOnMoveWorld)
+    }
+
+    private val keepAliveOnMoveWorld = object : EntityMoveWorldHandler {
+        override fun handle(event: EntityMoveWorldEvent): ActionResult {
+            val entity = event.entity
+
+            if (entity.world is ServerWorld && entity is MobEntityPersistentDurationMixin)
+                entity.setPersistentFor(PERSIST_DURATION)
+
+            return ActionResult.PASS
+        }
+    }
 }
