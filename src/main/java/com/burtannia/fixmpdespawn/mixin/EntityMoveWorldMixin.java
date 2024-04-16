8a87bad9c2ff267dc0bb129ff584bf20b8aab3c4
James Burton <jamesejburton@gmail.com>
2024-04-16 15:44:00 +0100

    Clean imports

--- src/main/java/com/burtannia/fixmpdespawn/mixin/EntityMoveWorldMixin.java
+++ src/main/java/com/burtannia/fixmpdespawn/mixin/EntityMoveWorldMixin.java
@@ -2,16 +2,13 @@ package com.burtannia.fixmpdespawn.mixin;
 
 import com.burtannia.fixmpdespawn.event.EntityMoveWorldEvent;
 import com.burtannia.fixmpdespawn.event.EntityMoveWorldHandler;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
-import net.minecraft.world.TeleportTarget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Entity.class)
 public class EntityMoveWorldMixin {
